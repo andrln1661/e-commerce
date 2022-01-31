2b50a9f3f8d3cc904cf8adbe080a98150d151c60
andrln1661 <andrln1661@gmail.com>
2022-01-31 19:15:04 +0300

    deploying

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/",
+  base: "/e-commerce/",
   build: {
     chunkSizeWarningLimit: 1600,
   },
