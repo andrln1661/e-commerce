99e81d233471c408654202a9c00bcfa254533430
andrln1661 <andrln1661@gmail.com>
2022-02-05 10:26:24 +0300

    triying to fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,4 +11,5 @@ export default defineConfig({
   server: {
     origin: "http://localhost:5000",
   },
+  
 });
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(serveStatic(path.join(__dirname, "/client/dist")));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.send(path.join(__dirname, "/client/dist", "index.html"));
   });
 }
