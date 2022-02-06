94f9c1e9c53edf87f972d4c1097bd88616d106ca
andrln1661 <andrln1661@gmail.com>
2022-02-07 00:12:47 +0300

    all

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
+import svgr from "@honkhonk/vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
